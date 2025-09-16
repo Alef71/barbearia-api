21db0272dd0171de3f601e722a7a49d1a814d1b8
alef <alefamaraltads@hotmail.com>
2025-09-16 11:34:37 -0300

    Implementa criptografia de senha com Spring Security

--- src/main/java/br/com/clubedosbarbas/barbeariaapi/service/BarbeiroService.java
+++ src/main/java/br/com/clubedosbarbas/barbeariaapi/service/BarbeiroService.java
@@ -3,6 +3,7 @@ package br.com.clubedosbarbas.barbeariaapi.service;
 import br.com.clubedosbarbas.barbeariaapi.model.Barbeiro;
 import br.com.clubedosbarbas.barbeariaapi.repository.BarbeiroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -12,7 +13,16 @@ public class BarbeiroService {
     @Autowired
     private BarbeiroRepository barbeiroRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public Barbeiro salvar(Barbeiro barbeiro) {
+        String senhaPura = barbeiro.getSenha();
+
+        String senhaCriptografada = passwordEncoder.encode(senhaPura);
+
+        barbeiro.setSenha(senhaCriptografada);
+
         return barbeiroRepository.save(barbeiro);
     }
 
